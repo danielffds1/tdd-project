6d05dda179d2ce847733bba49f3fd3c4b564848d
danielffds1 <danielffds8@gmail.com>
2023-05-05 11:38:30 -0300

    Implementando o Novo Projeto: Cada Lista Deve ter Uma URL

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from lists.models import Item, List
 
+
 def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
@@ -9,11 +10,12 @@ def home_page(request):
 
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    my_list = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=my_list)
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
     my_list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=my_list)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{my_list.id}/')
