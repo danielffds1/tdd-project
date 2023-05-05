b77ddda870d436b344be269635fd883714ed430a
danielffds1 <danielffds8@gmail.com>
2023-05-05 09:24:09 -0300

    Processando Requisição POST

--- functional_tests.py
+++ functional_tests.py
@@ -51,5 +51,9 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfeita, ela vai dormir
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+
 if __name__ == '__main__':
     unittest.main()
