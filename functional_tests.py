571ee69abe175b67caf8796d8de41093fa99c1b3
danielffds1 <danielffds8@gmail.com>
2023-05-05 09:25:40 -0300

    Passando variáveis no Template

--- functional_tests.py
+++ functional_tests.py
@@ -54,6 +54,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
 if __name__ == '__main__':
     unittest.main()
